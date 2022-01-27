9793f9f48f6288ece2a918c4a33957550df2195b
Abhay Chauhan <chauhan27.abhay@gmail.com>
2022-01-27 22:03:12 +0530

    user signup implemented

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,22 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        render plain: "Thanks"
+        # @user = User.new(params[:user])
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
 
 # @user -> instance variable -> going to be visible in our view
+# render plain: params -> {"utf8"=>"✓", "authenticity_token"=>"IA16PoBNWHeFNuKMWCMYGT9gVMy/47snqXmGK3rSAUh+vAPhCvpxCzCGJnD0QxP+rFX958uFvKTMojtoac2Mww==", "user"=>{"email"=>"abc@gmail.com", "password"=>"password", "password_confirmation"=>"password"}, "commit"=>"Create User", "controller"=>"registrations", "action"=>"create"}
+# render plain: params[:user] => {"email"=>"abc@gmail.com", "password"=>"password", "password_confirmation"=>"password"}
