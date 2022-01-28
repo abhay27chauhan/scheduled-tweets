2ff19b33d54f0f69c6725a6e0972f9f0da6a38ae
Abhay Chauhan <chauhan27.abhay@gmail.com>
2022-01-28 13:29:12 +0530

    shifted the session setting to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :set_current_user
+
+  def set_current_user
+    if session[:user_id]
+      @user_current = User.find_by(id: session[:user_id])
+    end
+  end
 end
