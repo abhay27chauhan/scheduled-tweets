16aed6f8a54a2c252e76eef456c80a1709fc3c56
Abhay Chauhan <chauhan27.abhay@gmail.com>
2022-01-28 12:22:34 +0530

    added flash and sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
-  delete "logout", to: "session#destroy"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
